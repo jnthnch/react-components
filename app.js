41394d70e5d299e05d01469c6b5649c7d597f946
jnthnch <jchu127@gmail.com>
2019-03-05 21:32:31 -0800

    implement beginning of app

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // TODO
+
+var groceryItems = ['Shrimp', 'Steak']
+
+var App = () => (
+    <div>
+        <h2>My Todo List</h2>
+        <GroceryList items={['Shrimp', 'Steak']} />
+    </div>
+)
+
+var GroceryList = (props) => (
+
+    <ul>
+        <li onClick={onListItemClick}><GroceryListItem item={props.items[0]}/></li>
+        <li onClick={onListItemClick}><GroceryListItem item={props.items[1]}/></li>  
+    </ul>
+)
+
+var onListItemClick = (event) => {
+    console.log('I got clicked');
+};
+    
+// var Shrimp = () => (
+//     <ul>
+//         <li>Shrimp</li>
+//     </ul>
+// )
+
+// var Steak = () => (
+//     <ul>
+//         <li>Steak</li>
+//     </ul>
+// )
+
+var GroceryListItem = (props) => (
+    <ul>
+        <li>{props.item}</li>
+    </ul>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"));
